8a7d037f1155e881c4485f3c67e94319f602cec1
abdulloh0512 <abdullohdev0512@gmail.com>
2024-05-15 03:40:56 +0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { useState, useEffect } from 'react'
-import Selector from './components/Selector'
-import Timer from './components/Timer'
-import styles from './App.module.css'
+import { useState, useEffect } from "react";
+import Selector from "./components/Selector";
+import Timer from "./components/Timer";
+import styles from "./App.module.css";
 
 function App() {
-  const [timeLimit, setTimeLimit] = useState(1200)
-  const [timeLeft, setTimeLeft] = useState(timeLimit)
-  const [isRunning, setIsRunning] = useState(false)
+  const [timeLimit, setTimeLimit] = useState(1200);
+  const [timeLeft, setTimeLeft] = useState(timeLimit);
+  const [isRunning, setIsRunning] = useState(false);
 
-  const [activeSelector, setActiveSelector] = useState('pomodoro')
+  const [activeSelector, setActiveSelector] = useState("pomodoro");
 
-  const [isShowing, setIsShowing] = useState(false)
+  const [isShowing, setIsShowing] = useState(false);
 
-  const [pomodoro, setPomodoro] = useState(1200)
-  const [shortBreak, setShortBreak] = useState(300)
-  const [longBreak, setLongBreak] = useState(900)
+  const [pomodoro, setPomodoro] = useState(1200);
+  const [shortBreak, setShortBreak] = useState(300);
+  const [longBreak, setLongBreak] = useState(900);
 
-  const [font, setFont] = useState('sans')
-  const [themeColor, setThemeColor] = useState('redOrange')
+  const [font, setFont] = useState("sans");
+  const [themeColor, setThemeColor] = useState("redOrange");
 
   const defaultModalSettings = {
     pomodoro,
@@ -25,41 +25,40 @@ function App() {
     longBreak,
     font,
     themeColor,
-  }
+  };
 
   useEffect(() => {
     if (isRunning && timeLeft > 0) {
-      const tID = setTimeout(() => setTimeLeft(timeLeft - 1), 1000)
+      const tID = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
       return () => {
-        clearTimeout(tID)
-      }
+        clearTimeout(tID);
+      };
     } else if (isRunning && timeLeft === 0) {
-      setIsRunning(false)
+      setIsRunning(false);
     }
-  })
-
+  });
 
   useEffect(() => {
-    if (activeSelector === 'pomodoro') {
-      setTimeLimit(pomodoro)
-    } else if (activeSelector === 'shortBreak') {
-      setTimeLimit(shortBreak)
-    } else if (activeSelector === 'longBreak') {
-      setTimeLimit(longBreak)
+    if (activeSelector === "pomodoro") {
+      setTimeLimit(pomodoro);
+    } else if (activeSelector === "shortBreak") {
+      setTimeLimit(shortBreak);
+    } else if (activeSelector === "longBreak") {
+      setTimeLimit(longBreak);
     }
-  }, [activeSelector, pomodoro, shortBreak, longBreak])
+  }, [activeSelector, pomodoro, shortBreak, longBreak]);
 
   useEffect(() => {
-    setIsRunning(false)
-    setTimeLeft(timeLimit)
-  }, [timeLimit])
+    setIsRunning(false);
+    setTimeLeft(timeLimit);
+  }, [timeLimit]);
 
   const handleClick = () => {
     if (timeLeft === 0) {
-      setTimeLeft(timeLimit)
+      setTimeLeft(timeLimit);
     }
-    setIsRunning(!isRunning)
-  }
+    setIsRunning(!isRunning);
+  };
 
   return (
     <div className={styles.App}>
@@ -79,7 +78,7 @@ function App() {
         themeColor={themeColor}
       />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
